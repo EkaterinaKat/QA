ffa0f51e3838a8cf95d97b12cd53d7b0efc02385
Екатерина <katyshevtseva_ekaterina@mail.ru>
2020-05-31 03:40:20 +0800

    Изменение структуры главного окна

--- src/controller/Catalogue.java
+++ src/controller/Catalogue.java
@@ -3,11 +3,15 @@ package controller;
 import database.JDBC;
 import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
+import model.QA;
+import utils.Utils;
 
+import java.util.ArrayList;
 import java.util.List;
 
 class Catalogue {
     private static Catalogue instance;
+    private Level level;
     private GridPane table;
 
     static Catalogue getInstance() {
@@ -20,34 +24,67 @@ class Catalogue {
 
     private Catalogue(GridPane table) {
         this.table = table;
+        level = Level._01;
         updateCatalogue();
     }
 
+    enum Level {_01, _2, _3}
+
     void updateCatalogue() {
         table.getChildren().clear();
-        List<String> questions = JDBC.getInstance().getQuestions();
+        List<QA> qas = getQAs();
         int row = 0;
-        for (String question : questions) {
-            addQuestionToCatalogue(question, row);
+        for (QA qa : qas) {
+            addQuestionToCatalogue(qa, row);
             row++;
         }
     }
 
-    private void addQuestionToCatalogue(String question, int row) {
-        Label label = new Label(question);
-        label.setOnMouseClicked(event -> ShowQA.show(question));
+    private List<QA> getQAs() {
+        List<QA> list = new ArrayList<>();
+        switch (level) {
+            case _01:
+                list = JDBC.getInstance().getQALevel_0_or_1();
+                break;
+            case _2:
+                list = JDBC.getInstance().getQALevel_2();
+                break;
+            case _3:
+                list = JDBC.getInstance().getQALevel_3();
+        }
+        return list;
+    }
+
+    private void addQuestionToCatalogue(QA qa, int row) {
+        Label label = new Label(Utils.getFullDescriptionOfQuestion(qa));
+        label.setOnMouseClicked(event -> ShowQA.show(qa));
         table.add(label, 0, row);
     }
 
     void updateCatalogue(String string) {
         table.getChildren().clear();
-        List<String> questions = JDBC.getInstance().getQuestions();
+        List<QA> qas = getQAs();
         int row = 0;
-        for (String question : questions) {
-            if (question.contains(string)) {
-                addQuestionToCatalogue(question, row);
+        for (QA qa : qas) {
+            if (qa.getQuestion().contains(string)) {
+                addQuestionToCatalogue(qa, row);
                 row++;
             }
         }
+    }
+
+    void show_01_level() {
+        level = Level._01;
+        updateCatalogue();
+    }
+
+    void show_2_level() {
+        level = Level._2;
+        updateCatalogue();
+    }
+
+    void show_3_level() {
+        level = Level._3;
+        updateCatalogue();
     }
 }
--- src/controller/Main.java
+++ src/controller/Main.java
@@ -1,14 +1,24 @@
 package controller;
 
+import controller.learn.Learn;
+import controller.learn.LearnMode012;
+import controller.learn.LearnMode23;
 import database.JDBC;
 import javafx.application.Application;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 import utils.WindowCreator;
 
 public class Main extends Application {
+    @FXML
+    private Button level_01_button;
+    @FXML
+    private Button level_2_button;
+    @FXML
+    private Button level_3_button;
     @FXML
     private TextField searchField;
     @FXML
@@ -34,6 +44,7 @@ public class Main extends Application {
         Catalogue.create(gridPane);
         searchField.textProperty().addListener((observable, oldValue, newValue) ->
                 Catalogue.getInstance().updateCatalogue(searchField.getText()));
+        makeButtonActive(level_01_button);
     }
 
     @FXML
@@ -41,7 +52,44 @@ public class Main extends Application {
         Adding.addQA();
     }
 
-    public void learn12ButtonListener() {
-        Learn.start(Learn.LearningMode.TO_2_LEVEL);
+    public void learn012ButtonListener() {
+        Learn.start(new LearnMode012());
     }
+
+    public void learn23ButtonListener() {
+        Learn.start(new LearnMode23());
+    }
+
+    @FXML
+    private void level01ButtonListener() {
+        makeButtonActive(level_01_button);
+        makeButtonInactive(level_2_button);
+        makeButtonInactive(level_3_button);
+        Catalogue.getInstance().show_01_level();
+    }
+
+    @FXML
+    private void level2ButtonListener() {
+        makeButtonActive(level_2_button);
+        makeButtonInactive(level_01_button);
+        makeButtonInactive(level_3_button);
+        Catalogue.getInstance().show_2_level();
+    }
+
+    @FXML
+    private void level3ButtonListener() {
+        makeButtonActive(level_3_button);
+        makeButtonInactive(level_2_button);
+        makeButtonInactive(level_01_button);
+        Catalogue.getInstance().show_3_level();
+    }
+
+    private void makeButtonActive(Button button) {
+        button.setDisable(true);
+    }
+
+    private void makeButtonInactive(Button button) {
+        button.setDisable(false);
+    }
+
 }
